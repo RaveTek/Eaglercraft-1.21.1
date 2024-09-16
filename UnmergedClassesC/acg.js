6f0f811520c1870f2fe00ffb5c96d7b42d0af4de
RaveTek <es55908@missionvistaacademy.org>
2024-09-16 13:04:07 -0700

    Create acg.js

---
+++ UnmergedClassesC/acg.js
@@ -0,0 +1,20 @@
+class ACG extends ZE {
+   constructor(iterable) {
+      super(iterable);
+   }
+
+   a() {
+      return AGG.a;
+   }
+
+   a(abu) {
+      abu.a(this);
+   }
+
+   // Synthetic method
+   // Bridge method
+   a(param1) {
+      this.a(param1);
+   }
+}
+
