ea1c480028306276e45d0b3a1084d57baa6ab87f
RaveTek <es55908@missionvistaacademy.org>
2024-09-16 13:00:27 -0700

    Create acb.js

---
+++ UnmergedClassesC/acb.js
@@ -0,0 +1,48 @@
+import { BiFunction } from 'some-library'; // Import any necessary libraries
+
+class Acb {
+    static a;
+    constructor(b, c, d) {
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+
+    static a($0, $1) {
+        const $2 = $0.i().H_();
+        return new Acb($0.aD_(), $0.r(), $1($0, $2));
+    }
+
+    static a($0) {
+        return Acb.a($0, (a, b) => a.a(b)); // Assuming dqh::a is a method reference
+    }
+
+    a() {
+        return Agg.i; // Assuming agg.i is a static property
+    }
+
+    a($0) {
+        $0.a(this);
+    }
+
+    b() {
+        return this.b;
+    }
+
+    e() {
+        return this.c;
+    }
+
+    f() {
+        return this.d;
+    }
+
+    a(var1) {
+        this.a(var1);
+    }
+
+    static {
+        Acb.a = Yx.a(Jd.b, Acb.b, Yv.a(Lu.h), Acb.e, Yv.p, Acb.f, Acb);
+    }
+}
+
