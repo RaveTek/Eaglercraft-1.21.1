9b5361ff62fff7290012d0831b7cd4abc0582534
RaveTek <es55908@missionvistaacademy.org>
2024-09-16 13:09:26 -0700

    Create acj.js

---
+++ UnmergedClassesC/acj.js
@@ -0,0 +1,25 @@
+import { ByteBuf } from 'io.netty.buffer';
+
+class Acj {
+    static a = new Acj();
+    static b;
+
+    constructor() {}
+
+    a() {
+        return Agg.o;
+    }
+
+    a(abu) {
+        abu.a(this);
+    }
+
+    // Synthetic method
+    a(param1) {
+        this.a(param1);
+    }
+
+    static {
+        Acj.b = Yx.a(Acj.a);
+    }
+}
