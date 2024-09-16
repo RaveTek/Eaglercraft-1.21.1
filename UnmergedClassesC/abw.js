8be42d58ac431c81c406c642f7f6db4ae63b0ff3
RaveTek <es55908@missionvistaacademy.org>
2024-09-16 12:08:28 -0700

    Create abw.js

---
+++ UnmergedClassesC/abw.js
@@ -0,0 +1,65 @@
+class abw implements zg<abu> {
+   static final yx<vw, abw> a = zg.a(abw::a, abw::new);
+   private final int b;
+   private final double c;
+   private final double d;
+   private final double e;
+   private final int f;
+
+   constructor(btc $$0, aqt $$1) {
+      this.b = $$0.an();
+      const $$2 = $$1.b();
+      this.c = $$2.a();
+      this.d = $$2.b();
+      this.e = $$2.c();
+      this.f = $$0.p();
+   }
+
+   constructor(vw $$0) {
+      this.b = $$0.l();
+      this.c = $$0.readDouble();
+      this.d = $$0.readDouble();
+      this.e = $$0.readDouble();
+      this.f = $$0.readShort();
+   }
+
+   a(vw $$0) {
+      $$0.c(this.b);
+      $$0.a(this.c);
+      $$0.a(this.d);
+      $$0.a(this.e);
+      $$0.l(this.f);
+   }
+
+   a() {
+      return agg.d;
+   }
+
+   a(abu $$0) {
+      $$0.a(this);
+   }
+
+   b() {
+      return this.b;
+   }
+
+   e() {
+      return this.c;
+   }
+
+   f() {
+      return this.d;
+   }
+
+   g() {
+      return this.e;
+   }
+
+   h() {
+      return this.f;
+   }
+
+   a(wf param1) {
+      this.a(param1);
+   }
+}
