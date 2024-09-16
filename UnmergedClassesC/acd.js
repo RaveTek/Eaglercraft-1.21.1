a32ef4f3614d4e2594d4295705ff60506f493253
RaveTek <es55908@missionvistaacademy.org>
2024-09-16 13:02:33 -0700

    Create acd.js

---
+++ UnmergedClassesC/acd.js
@@ -0,0 +1,34 @@
+class ACD {
+    static a = YX.a(JD.b, ACD.e, YV.a(DFY.q), ACD.b, ACD.new);
+
+    constructor($0, $1) {
+        this.b = $0;
+        this.c = $1;
+    }
+
+    constructor($0, $1) {
+        this($1, $0.a_($1));
+    }
+
+    a() {
+        return AGG.k;
+    }
+
+    a($0) {
+        $0.a(this);
+    }
+
+    b() {
+        return this.c;
+    }
+
+    e() {
+        return this.b;
+    }
+
+    // Synthetic method
+    a(param1) {
+        this.a(param1);
+    }
+}
+
