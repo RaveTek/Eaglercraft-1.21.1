108b2c335c558e960f4bdd22bfc00ba87b19435d
AR-DEV-1 <mail.ardev1@protonmail.com>
2024-10-02 16:44:21 +0500

    Update audio_player.js

--- audio_player.js
+++ audio_player.js
@@ -1,14 +1,9 @@
+       // Get the audio element
+        const audio = document.getElementById('background-audio');
 
-    // Path to the single audio file
-    const audioFile = "https://github.com/AR-DEV-1/Eaglercraft-1.21.1/blob/main/assets/minecraft/sounds/title_screen/Minecraft.mp3";  // minecraft.mp3
+        // Play audio when the page loads
+        window.onload = function() {
+            audio.play();
+        };
 
-    // Create an audio element and set its attributes
-    const audio = new Audio(audioFile);
-    audio.loop = true;   // Lol
-    audio.autoplay = true; // Stuff
 
-    // Play the audio when the page loads
-    window.onload = function() {
-        audio.play();
-    };
-// ;) Copyright no one. Not even you MOJANG!
