1a6fa25d746e55b37fe96e389495591bab3ac511
RaveTek <es55908@missionvistaacademy.org>
2024-10-23 19:22:15 -0700

    Create registry.js

---
+++ registry.js
@@ -0,0 +1,11 @@
+window.onload function {
+  game: 
+  --username: Player,
+    --version: 1.21.1,
+    --gameDir: .minecraft/,
+    --assetsDir: assets/,
+    --assetIndex: assets.js,
+    --uuid: a701b970-e3ba-4bb1-8664-c4ecc6e2f5ba,
+    --accessToken: null,
+    --clientId: null,
+    }
